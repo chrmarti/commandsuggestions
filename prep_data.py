9f4e23d93e4ad4c7dc8b41936d1b15ae6f944ba2
Christof Marti <chrmarti@microsoft.com>
2017-07-24 18:13:37 -0700

    Run for specific machine id

--- prep_data.py
+++ prep_data.py
@@ -1,32 +1,45 @@
 import gzip
 import csv
 import numpy as np
+# from scipy.sparse import lil_matrix, save_npz
+import time
+
+start = time.time()
 
 filename = 'dumps/single-day-20170719.gz'
 
 machines = {}
 commands = {}
 maxCount = 0
+
+lines = None
 with gzip.open(filename, mode='rt') as f:
 	csvobj = csv.reader(f, delimiter = ',', quotechar='"')
-	for line in csvobj:
-		machine = line[2]
-		command = line[1]
-		if command not in commands:
-			commands[command] = len(commands)
-		if machine not in machines:
-			machines[machine] = len(machines)
-		maxCount = max(maxCount, int(line[0]))
+	lines = list(csvobj)
+print('tuples', len(lines))
+print('loaded', time.time() - start)
+
+for line in lines:
+	machine = line[2]
+	command = line[1]
+	if command not in commands:
+		commands[command] = len(commands)
+	if machine not in machines:
+		machines[machine] = len(machines)
+	maxCount = max(maxCount, int(line[0]))
 
 print('maxCount', maxCount)
+print('dims', (len(machines), len(commands)))
+print('first run', time.time() - start)
 
+# X = lil_matrix((len(machines), len(commands)), dtype=np.int32)
 X = np.zeros((len(machines), len(commands)), dtype=np.int32)
-with gzip.open(filename, mode='rt') as f:
-	csvobj = csv.reader(f, delimiter = ',', quotechar='"')
-	for line in csvobj:
-		machine = line[2]
-		command = line[1]
-		X[machines[machine], commands[command]] += 1
+for line in lines:
+	machine = line[2]
+	command = line[1]
+	X[machines[machine], commands[command]] += 1
+
+print('second run', time.time() - start)
 
 import os
 if not os.path.exists('data'):
@@ -34,5 +47,9 @@ if not os.path.exists('data'):
 
 from sklearn.externals import joblib
 joblib.dump(machines, 'data/machines.pickle')
+print('machines saved', time.time() - start)
 joblib.dump(commands, 'data/commands.pickle')
+print('commands saved', time.time() - start)
+# save_npz('data/data.npz', X)
 np.savez_compressed('data/data.npz', X=X)
+print('data saved', time.time() - start)
--- test.py
+++ test.py
@@ -1,16 +1,44 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.externals import joblib
+import time
+
+start = time.time()
+
+machine_id = ''
 
 machines = joblib.load('data/machines.pickle')
+machine_index = machines[machine_id]
+print('machines loaded', time.time() - start)
 commands = joblib.load('data/commands.pickle')
+print('commands loaded', time.time() - start)
 X = np.load('data/data.npz')['X']
+print('data loaded', time.time() - start)
 
-print('loaded')
-
-nbrs = NearestNeighbors(n_neighbors=2, algorithm='brute').fit(X)
+nbrs = NearestNeighbors(n_neighbors=100, algorithm='brute', n_jobs=-1).fit(X)
 # nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
-distances, indices = nbrs.kneighbors(X[0].reshape(1, -1))
+print('fit', time.time() - start)
+input = X[machine_index]
+distances, mindices = nbrs.kneighbors(input.reshape(1, -1))
 
-print(distances)
-print(indices)
+print('distances', distances)
+print('mindices', mindices)
+print('kneighbors', time.time() - start)
+
+candidates = {}
+for mindex in mindices[0][1:]:
+	candidate = X[mindex]
+	for cindex in range(0, len(commands)):
+		if input[cindex] == 0 and candidate[cindex] != 0:
+			if cindex not in candidates:
+				candidates[cindex] = { 'index' : cindex, 'count' : 0 }
+			candidates[cindex]['count'] += candidate[cindex]
+print('candidates', time.time() - start)
+
+commands_inv = { v: k for k, v in commands.items() }
+print('commands_inv', time.time() - start)
+
+result = sorted(list(candidates.values()), key=lambda k: -k['count'])
+for candidate in result:
+	print(commands_inv[candidate['index']], candidate['count'])
+print('result', time.time() - start)
